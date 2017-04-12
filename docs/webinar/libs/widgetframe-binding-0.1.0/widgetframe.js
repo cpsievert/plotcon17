350c16836575a504746064644f7f9106f8bccbf3
Carson Sievert <cpsievert1@gmail.com>
2017-04-11 23:33:59 -0500

    webinar slides

---
+++ docs/webinar/libs/widgetframe-binding-0.1.0/widgetframe.js
@@ -0,0 +1,22 @@
+HTMLWidgets.widget({
+
+  name: 'widgetframe',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    return {
+
+      renderValue: function(x) {
+         HTMLWidgets.pymParent = new pym.Parent(el.id, x.url,
+          x.options || {});
+      },
+
+      resize: function(width, height) {
+
+      }
+
+    };
+  }
+});
